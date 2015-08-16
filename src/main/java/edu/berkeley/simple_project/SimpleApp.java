42a107976a1fe659dde49d86f4e0ed0dec385052
timothy22000 <timothy22000@gmail.com>
2015-08-16 22:05:08 +0100

    committing files

---
+++ src/main/java/edu/berkeley/simple_project/SimpleApp.java
@@ -0,0 +1,25 @@
+package edu.berkeley.simple_project;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+public class SimpleApp {
+	public static void main(String[] args){
+		String logFile = "~/Downloads/spark-1.4.0-bin-hadoop2.6/README.md";
+		SparkConf conf = new SparkConf().setAppName("Simple Application");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> logData = sc.textFile(logFile).cache();
+		
+		long numAs = logData.filter(new Function<String, Boolean>(){
+			public Boolean call(String s){return s.contains("a");}
+		}).count();
+		
+		long numBs = logData.filter(new Function<String, Boolean>(){
+			public Boolean call(String s){return s.contains("b");}
+		}).count();
+				
+		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+	}
+}
